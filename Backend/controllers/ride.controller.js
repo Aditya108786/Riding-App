f18e102158af350ad31a0c1ef5af6aeae18ee43a
aditya singh <aditya.23cse362@rtu.ac.in>
2026-01-30 03:40:38 +0530

    updated confirmride controller4

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -104,17 +104,17 @@ module.exports.ConfirmRide = async(req, res) => {
       console.log("⚠️ Captain has no socketId");
     }
      // console.log(ride.user.socketId)
-      console.log(ride.ridewithuser.user.socketId)
+      //console.log(ride.ridewithuser.user.socketId)
     // Notify user to start chat
-    io.to(ride.ridewithuser.user.socketId).emit("start:chat", roomId);
+    io.to(ride.user.socketId).emit("start:chat", roomId);
     console.log(`📢 Sent start:chat to user: ${ride.user.socketId}`);
 
     // Notify user that ride is confirmed
-    io.to(ride.ridewithuser.user.socketId).emit("ride-confirmed", {
+    io.to(ride.user.socketId).emit("ride-confirmed", {
       message: "Ride accepted",
       ride
     });
-    console.log(`✅ Sent ride-confirmed to user: ${ride.ridewithuser.user.socketId}`);
+    console.log(`✅ Sent ride-confirmed to user: ${ride.user.socketId}`);
 
     return res.status(200).json({ roomId, ride });
     

71c9ddacaae94b9f8e8502ae2ea7803c6004dc63
aditya singh <aditya.23cse362@rtu.ac.in>
2026-01-31 13:09:18 +0530

    redis connectivity

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,22 @@
+const app = require('./app');
+const http = require('http');
+const { connectRedis } = require('./config/Redis');
+const { initializesocket } = require('./socket');
 
-const app = require('./app')
-const http = require('http')
-const {connectRedis} = require("./config/Redis")
-const {initializesocket, sendmessagetosocketid} = require('./socket')
+const PORT = process.env.PORT || 5000;
 
-const port = process.env.PORT
+const server = http.createServer(app);
 
-const server = http.createServer(app)
- 
-(async()=>{
-    try {
-        await connectRedis()
-        await initializesocket(server)
-    }catch (err){
-        console.error("Startup failed",err)
-        process.exit(1)
-    }
-})()
+(async () => {
+  try {
+    await connectRedis();
+    await initializesocket(server);
 
-server.listen(process.env.PORT || 5000 , ()=>{
-     console.log("Server running on port 5000")
-})
+    server.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Startup failed:", err);
+    process.exit(1);
+  }
+})();
